28b083eda186c160a0b23bdc3dd138a71c7205ea
Babgsuke <bangsuke74@gmail.com>
2025-12-12 18:28:15 +0700

    Update createSshController.js

--- controller/createSshController.js
+++ controller/createSshController.js
@@ -4,7 +4,7 @@ const chars = "abcdefghijklmnopqrstuvwxyz0123456789";
 
 function uniq8() {
   let result = "";
-  for (let i = 0; i < 8; i++) {
+  for (let i = 0; i < 4; i++) {
     result += chars.charAt(Math.floor(Math.random() * chars.length));
   }
   return result;
